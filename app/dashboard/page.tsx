4dc86acf27c7902157bde45fc8b9ac7b867be657
Ajarek <ajarek@poczta.onet.pl>
2025-06-01 01:07:24 +0200

    added auth mongodb mongoose

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { auth } from '@/app/api/auth/auth'
+import { redirect } from 'next/navigation'
+import Image from 'next/image'
+import { Bell } from 'lucide-react'
+import Container from '@/components/Container'
+import LogoutBtn from '@/components/LogoutBtn'
+
+const Dashboard = async () => {
+  const session = await auth()
+  if (!session) {
+    redirect('/')
+  }
+  return (
+    <div>
+      <div className='w-full h-[53px] bg-primary flex items-center justify-start px-4 text-white gap-4 '>
+        <Image
+          src={session?.user?.image || ''}
+          alt='hero image'
+          width={40}
+          height={40}
+          className='rounded-full'
+        />
+        <h1 className='text-xl font-semibold'>
+          Hi {session?.user?.name || ''}
+        </h1>
+        <div className='relative flex items-center'>
+          <Bell />
+          <div className='absolute -right-2 -top-2 w-5 h-5 rounded-full bg-red-500 flex items-center justify-center'>
+            3
+          </div>
+        </div>
+        <LogoutBtn/>
+      </div>
+      <Container>
+        <h1 className='m-4'>Dashboard</h1>
+      </Container>
+    </div>
+  )
+}
+
+export default Dashboard
